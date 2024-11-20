6a40ec65df1a457fc1ed5bccf4cc1cd81617d280
imufwt <elves.2010@outlook.com>
2024-11-20 22:35:52 +0800

    redis key expire time need typtOf int

--- bot/chats/roles/record.ts
+++ bot/chats/roles/record.ts
@@ -10,7 +10,7 @@ export default [{
     const now = new Date();
     // 第二天的0点时间
     const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
-    // 毫秒数差，并转换为秒
+    // 毫秒数差，并转换为秒 转化为整数
     const diff = (tomorrow.getTime() - now.getTime()) / 1000;
     // 查询 标识
     client.get(senderUserName).then(async (key: string | null) => {
@@ -18,7 +18,7 @@ export default [{
       if (!key) {
         fishpi.chatroom.send("凌 发红包 " + senderUserName + " "+(Math.floor(Math.random() * 64)+1)+" 行行行,好好好!", ClientType.ElvesOnline, "精灵互联");
         await client.set(senderUserName, "1");
-        await client.expire(senderUserName, diff);
+        await client.expire(senderUserName, Number(diff));
         return;
       }
       // 否则就提示已经存在
